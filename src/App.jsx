eef4998791d6856198c5bf791b390d79fd3b8608
Sumant.xD <st72365@gmail.com>
2023-01-28 11:14:09 +0530

    half way

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   let options = {
     appId: "4871916f1508465c9d097ec220d87697",
     channel: "sumant",
-    token: "007eJxTYLjAUqQeWxz813F62+k7Jf6NEUz8oR5PTk+U1tIzN5Y2CFFgMLEwN7Q0NEszNDWwMDEzTbZMMbA0T002MjJIsTA3szSvDb+S3BDIyMBe7M3CyACBID4bQ3FpbmJeCQMDAGAoG68=",
+    token: "007eJxTYJgTqXTPboX93tpM0XM3tsWZGX4o2LB6onj8yjvaziqikRsUGEwszA0tDc3SDE0NLEzMTJMtUwwszVOTjYwMUizMzSzNXZddSW4IZGQ4veQZAyMUgvhsDMWluYl5JQwMAEDsH6Q=",
     uid: 0,
   };
 
@@ -23,21 +23,26 @@ export default function App() {
   //we will create our refs here
 
   const agoraEngine = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-  const remotePlayerContainer = document.createElement("div");
-  const localPlayerContainer = document.createElement("div");
+  //we have created the remote player conatiner //and local player container manually 
+  //all we need to do is to refer it
+
+  // const remotePlayerContainer = document.createElement("div");
+  const remotePlayerContainer = useRef(null);
+  // const localPlayerContainer = document.createElement("div");
+  const localPlayerContainer = useRef(null);
 
   async function startBasicCall() {
 
-    localPlayerContainer.id = options.uid;
-    localPlayerContainer.textContent = "Local user " + options.uid;
+    localPlayerContainer.current.id = options.uid;
+    // localPlayerContainer.current.textContent = "Local user " + options.uid;
     // Set the local video container size.
-    localPlayerContainer.style.width = "640px";
-    localPlayerContainer.style.height = "480px";
-    localPlayerContainer.style.padding = "15px 5px 5px 5px";
+    // localPlayerContainer.current.style.width = "640px";
+    // localPlayerContainer.current.style.height = "480px";
+    localPlayerContainer.current.style.padding = "3px";
     // Set the remote video container size.
-    remotePlayerContainer.style.width = "640px";
-    remotePlayerContainer.style.height = "480px";
-    remotePlayerContainer.style.padding = "15px 5px 5px 5px";
+    // remotePlayerContainer.current.style.width = "640px";
+    // remotePlayerContainer.current.style.height = "480px";
+    remotePlayerContainer.current.style.padding = "3px";
 
 
     agoraEngine.on("user-published", async (user, mediaType) => {
@@ -50,13 +55,15 @@ export default function App() {
         // Save the remote user id for reuse.
         channelParameters.remoteUid = user.uid.toString();
         // Specify the ID of the DIV container. You can use the uid of the remote user.
-        remotePlayerContainer.id = user.uid.toString();
+        remotePlayerContainer.current.id = user.uid.toString();
         channelParameters.remoteUid = user.uid.toString();
-        remotePlayerContainer.textContent = "Remote user " + user.uid.toString();
+        // remotePlayerContainer.current.textContent = "Remote user " + user.uid.toString();
         // Append the remote container to the page body.
-        document.body.append(remotePlayerContainer);
+        // document.body.append(remotePlayerContainer);
         // Play the remote video track.
-        channelParameters.remoteVideoTrack.play(remotePlayerContainer);
+        //inside the play we have to pass in the container where we want to play the video
+        //just forget about the wrapper class and implement it hard core. ******************************
+        channelParameters.remoteVideoTrack.play(remotePlayerContainer.current);
       }
 
       if (mediaType == "audio") {
@@ -87,14 +94,14 @@ export default function App() {
     channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
     channelParameters.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
     // Append the local video container to the page body.
-    document.body.append(localPlayerContainer);
+    // document.body.append(localPlayerContainer.current);
     // Publish the local audio and video tracks in the channel.
     await agoraEngine.publish([
       channelParameters.localAudioTrack,
       channelParameters.localVideoTrack,
     ]);
     // Play the local video track.
-    channelParameters.localVideoTrack.play(localPlayerContainer);
+    channelParameters.localVideoTrack.play(localPlayerContainer.current);
     console.log("publish success!");
   };
 
@@ -102,8 +109,8 @@ export default function App() {
     channelParameters.localAudioTrack.close();
     channelParameters.localVideoTrack.close();
     // Remove the containers you created for the local video and remote video.
-    removeVideoDiv(remotePlayerContainer.id);
-    removeVideoDiv(localPlayerContainer.id);
+    // removeVideoDiv(remotePlayerContainer.id);
+    // removeVideoDiv(localPlayerContainer.id);
     // Leave the channel
     await agoraEngine.leave();
     console.log("You left the channel");
@@ -132,8 +139,12 @@ export default function App() {
       </div>
 
       <div className=" flex-col">
-          <div className="  bg-slate-200 w-1/2 mx-auto mt-10 h-[450px] border-2 border-dashed border-slate-700"> 
+        <div className=" flex bg-slate-400">
+          <div ref={remotePlayerContainer} className="  bg-slate-200 w-1/2 mx-auto mt-10 h-[450px] border-2 border-dashed border-slate-700 relative left-[87px]"> 
           </div>
+          <div ref={localPlayerContainer} className=" bg-slate-800 w-44 h-44 border-2 border-dashed rounded-md mt-10 relative right-[55px]">
+          </div>
+        </div>
 
           <div className=" bg-slate-300 w-1/2 mx-auto mt-2 flex justify-around">
 
